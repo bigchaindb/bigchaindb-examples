62c35d34e34d9c809991773a8b9097fb3014fd84
Rodolphe Marques <rodolphe@ascribe.io>
2016-07-06 19:03:54 +0200

    initial implementation with websockets

---
+++ tornado.py
@@ -0,0 +1,25 @@
+from tornado import websocket, ioloop, web
+
+clients = []
+
+class StreamHandler(websocket.WebSocketHandler):
+
+    def open(self):
+        if self not in cl:
+            cl.append(self)
+            self.write_message('Hello')
+            print('Received connection from {}'.format(self))
+
+    def on_close(self):
+        if self in clients:
+            clients.remove(self)
+            print('Connection closed for {}'.format(self))
+
+app = web.Application([
+    (r'/ws', StreamHandler),
+])
+
+if __name__ == '__main__':
+    app.listen(8888)
+    print('Starting tornado')
+    ioloop.IOLoop.instance().start()
