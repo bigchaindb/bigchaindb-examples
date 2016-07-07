45572a6a033ff51a7c5f03bac363fe5f79c8054e
Rodolphe Marques <rodolphe@ascribe.io>
2016-07-07 14:01:24 +0200

    fixed html and tornado

--- tornado.py
+++ tornado.py
@@ -1,6 +1,7 @@
 from bigchaindb import Bigchain
 from tornado import websocket, ioloop, web
 from tornado.gen import coroutine
+from tornado import gen
 import rethinkdb as r
 import time
 import json
@@ -8,6 +9,7 @@ import json
 clients = []
 coins_seen = []
 play_until = time.time()
+r.set_loop_type('tornado')
 
 
 class StreamHandler(websocket.WebSocketHandler):
@@ -18,7 +20,6 @@ class StreamHandler(websocket.WebSocketHandler):
     def open(self):
         if self not in clients:
             clients.append(self)
-            self.write_message('Hello')
             print('Received connection from {}'.format(self))
 
     def on_close(self):
@@ -39,6 +40,9 @@ def get_coin_from_block(block):
 
 @coroutine
 def play():
+    print('Entering Play')
+    print(play_until)
+    print(coins_seen)
     playing = False
     while True:
         if play_until > time.time() and playing is False:
@@ -55,22 +59,23 @@ def play():
 
 @coroutine
 def listen_payments():
+    print('Entering listen_payments')
+    global play_until
+    print(coins_seen)
+    print(play_until)
     b = Bigchain()
-    conn = b.conn
+    conn = yield b.conn
     feed = yield r.table('bigchain').changes().run(conn)
 
     while (yield feed.fetch_next()):
-        block = feed.next()
-        coins = get_coin_from_block(block)
+        block = yield feed.next()
+        coins = get_coin_from_block(block['new_val'])
         if coins:
             print('received coins')
         if time.time() > play_until:
             play_until = time.time() + 10 * len(coins)
         else:
             play_until += 10 * len(coins)
-
-
-
 
 
 app = web.Application([
@@ -80,6 +85,7 @@ app = web.Application([
 if __name__ == '__main__':
     app.listen(8888)
     print('Starting tornado')
+    print(play_until)
     ioloop.IOLoop.current().add_callback(play)
     ioloop.IOLoop.current().add_callback(listen_payments)
     ioloop.IOLoop.instance().start()
