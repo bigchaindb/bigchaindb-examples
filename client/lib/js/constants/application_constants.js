ded92bf6cb4aab64fc3468978bbf4d901634c176
Brett Sun <qisheng.brett.sun@gmail.com>
2016-07-14 17:40:46 +0200

    Remove unused tornado configuration in client
    The server now sends back the plugin addresses for each account so this
    is unnecessary.

--- client/lib/js/constants/application_constants.js
+++ client/lib/js/constants/application_constants.js
@@ -1,10 +1,7 @@
 export const FLASK_BASE_URL = process.env.FLASK_BASE_URL;
-export const TORNADO_BASE_URL = process.env.TORNADO_BASE_URL;
-
 export const API_PATH = `${FLASK_BASE_URL}/api/`;
 
 export default {
     API_PATH,
     FLASK_BASE_URL,
-    TORNADO_BASE_URL
 };
